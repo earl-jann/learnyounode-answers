7a838a4425240e257fc7b0b7a09d329aa68da7ea
earl-jann <earljanntormes@gmail.com>
2017-01-26 15:50:23 +0800

    program9

--- program9.js
+++ program9.js
@@ -1,1 +1,29 @@
 // Juggling async
+var http = require('http');
+var concatStream = require('concat-stream');
+
+var urls = [process.argv[2],process.argv[3],process.argv[4]];
+
+var myResults = [];
+var myCbCounter = 0;
+
+function wrapHttpGet(index) {	
+	http.get(urls[index], function(response) {
+		response.pipe(concatStream(function(data) {
+
+			myResults[index] = data.toString();
+			myCbCounter++;			
+
+			if (myCbCounter == 3) {
+				for (var i = 0; i < 3; i++) {
+					console.log(myResults[i]);
+				}		
+			}
+		}));
+	});
+}
+
+for (var i = 0; i < 3; i++) {
+	//console.log(urls[i]);
+	wrapHttpGet(i);
+}
